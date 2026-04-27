62c5b6217e965ca9bf0d90dcac02d27dfbadf48c
Bekzat <bekzatberk157@gmail.com>
2026-04-27 11:23:57 +0500

    tap1

--- test/MainTest.java
+++ test/MainTest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class MainTest {
     public static void main (String args[]) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Sandy engiziniz: ");
+        System.out.print("Sandy engiziniz: ");
         int a = sc.nextInt();
         if (a % 2 == 0) {
             System.out.println("Even");
